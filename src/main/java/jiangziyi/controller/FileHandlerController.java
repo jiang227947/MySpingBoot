6aa66779c3b40dc1a2903421935f4d26df34e266
jzywd <jzywd666@163.com>
2023-01-17 17:47:01 +0800

    [M] 1. 下载地址修改

--- src/main/java/jiangziyi/controller/FileHandlerController.java
+++ src/main/java/jiangziyi/controller/FileHandlerController.java
@@ -1,5 +1,6 @@
 package jiangziyi.controller;
 
+import jiangziyi.comstant.DocumentConstant;
 import jiangziyi.pojo.query.PageParams;
 import jiangziyi.service.FileService;
 import jiangziyi.sys.ResultObj;
@@ -49,7 +50,7 @@ public class FileHandlerController {
     @PostMapping("/download")
     @ResponseBody
     public void download(@RequestBody String filename, HttpServletResponse response) {
-        File file = new File("D:/static/files/" + filename);
+        File file = new File(DocumentConstant.FILE_ADDRESS + filename);
         String fileName = file.getName();
         String fileAbsolutePath = file.getAbsolutePath();
         try (FileInputStream inputStream = new FileInputStream(fileAbsolutePath);
